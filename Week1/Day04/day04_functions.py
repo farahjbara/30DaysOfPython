035fbd2c9d3d9eaa9820f1d1232eca4347317558
fjbara <farahjbara1@gmail.com>
2025-04-06 22:19:11 +0100

    Day04

---
+++ Week1/Day04/day04_functions.py
@@ -0,0 +1,4 @@
+# Day 05 – Python Functions
+#✅ What is a Function?
+#A function is a reusable block of code that performs a specific task. Instead of writing the same code over and over, we wrap it in a function and call it whenever we need it.
+
